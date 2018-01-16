e5203fb6968539ae48f2c8dc77366b6380131598
erwan <erwan.guillet44@hotmail.fr>
2018-01-16 13:33:00 +0100

    Add route single user and refactoring

--- imiebook/src/AppBundle/Controller/UsersController.php
+++ imiebook/src/AppBundle/Controller/UsersController.php
@@ -6,26 +6,46 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest; // alias pour toutes les annotations
 
 use AppBundle\Entity\Users;
 
 class UsersController extends Controller
 {
 
+    /**
+     * @Rest\View()
+     * @Rest\Get("/users")
+     */
     public function getUsersAction(Request $request)
     {
         $users = $this->getDoctrine()
             ->getRepository(Users::class)
             ->findAll();
-        // $users = $usersRepo->findAll();
         $tabUsers = [];
 
         foreach($users as $key => $user) {
             $tabUsers[] = $user->toArray();
         }
 
+        return $users;
+    }
 
-        return new JsonResponse($tabUsers);
+    /**
+     * @Rest\View()
+     * @Rest\Get("/users/{id}")
+     */
+    public function getUserAction(Request $request)
+    {
+        $user = $this->getDoctrine()
+            ->getRepository(Users::class)
+            ->find($request->get('id'));
+
+        if (empty($user)) {
+            return new JsonResponse(['message' => 'Not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $user;
     }
 
 }
