14703562cfcafb662ac0b6c2e045c27ff985b83b
root <imieUmake@gmail.com>
2018-03-05 14:37:20 +0100

    Degres API

--- imiebook/src/AppBundle/Controller/DegresController.php
+++ imiebook/src/AppBundle/Controller/DegresController.php
@@ -26,10 +26,53 @@ class DegresController extends Controller
             ->find($request->get('id'));
 
         if (empty($user)) {
-            return new JsonResponse(['message' => 'Not found degres'], Response::HTTP_NOT_FOUND);
+            return new JsonResponse(['message' => 'Degres not found'], Response::HTTP_NOT_FOUND);
         }
 
         return $user->getDegres();
     }
+
+    /**
+     * @Rest\View()
+     * @Rest\Get("/degres/{id}")
+     */
+    public function getDegreAction(Request $request){
+
+        $degre = $this->getDoctrine()
+            ->getRepository(Degres::class)
+            ->find($request->get('id'));   
+        if (empty($degre)){
+            return new JsonResponse(['message' => 'Degre not found'], Response::HTTP_NOT_FOUND);
+        }
+        return $degre;
+    }
+
+    /**
+     * @Rest\View()
+     * @Rest\Post("/degres")
+     */
+    public function postDegreAction(Request $request){
+
+        $user = $this->get('security.token_storage')
+            ->getToken()
+            ->getUser();
+
+        $degre = new Degres;
+        $degre->setUser($user);
+        $form = $this->createForm(DegresValidator::class, $degre);
+
+        $form->submit($request->request->all()); // Validation des données
+
+        if ($form->isValid()){
+           
+            $em = $this->get('doctrine.orm.entity_manager');
+            $em->persist($degre);
+            $em->flush();
+            return $degre;
+        }
+        // if error return explication
+        return $form;
+    }
+
 
 }
