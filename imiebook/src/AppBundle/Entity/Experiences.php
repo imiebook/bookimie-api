070d2070d9a4894f0fed3760d7915742b975ae2c
Miratons <erwan.guillet44@hotmail.fr>
2018-06-05 11:58:14 +0200

    Revert "Egt elk"

--- imiebook/src/AppBundle/Entity/Experiences.php
+++ imiebook/src/AppBundle/Entity/Experiences.php
@@ -4,7 +4,6 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\DBAL\Types\DateTimeType;
-use FOS\ElasticaBundle\Configuration\Search;
 
 /**
  * @ORM\Table(name="experiences")
